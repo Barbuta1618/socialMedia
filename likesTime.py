b0d5dc7c7fe77f7750450590d614ba76ec515151
Barbuta Cristian <barbuta.cristian1618@gmail.com>
2021-11-06 23:30:47 +0200

    added likesTime.py

--- likesTime.py
+++ likesTime.py
@@ -5,37 +5,25 @@ from datetime import datetime
 def likesTime():
     # Opening JSON file
     f = open('./datasets/facebook.json', encoding="utf8")
-    
-    # returns JSON object as
-    # a dictionary
     data = json.load(f)
     
-    # Iterating through the json
-    # list
-    # for i in data["likes"]:
-    # print(data)
-    
-    ts = int(data[0]['created_at'])
-    print(datetime.utcfromtimestamp(ts).strftime('%Y-%m-%d %H:%M:%S'))
-    
-    likesList = []
-    for post in data:
-        likesList.append(post["likes"])
-    
-    # sort likesList
-    likesList.sort()
-    
-    
     likes = []
+    shares = []
+    comms = []
     for i in range(24):
         likes.append(0)
-    
+        shares.append(0)
+        comms.append(0)
+
     for post in data:
         ts = int(post['created_at'])
         date = int(datetime.utcfromtimestamp(ts).strftime('%H'))
         likes[date] = int(post['likes']) + likes[date]
+        shares[date] = int(post['shares']) + shares[date]
+        comms[date] = int(post['reactions']) + comms[date]
     
+
     
     # Closing file
     f.close()
-    return likes
+    return (likes, shares, comms)
